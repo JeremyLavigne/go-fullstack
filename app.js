2e84de61b1e4023f347bf3fc67fca13baadfe009
JeremyLavigne <lavigne.jeremy@post.com>
2020-10-24 15:53:46 +0200

    Create basics GET/POST requests.

--- app.js
+++ app.js
@@ -1,27 +1,43 @@
 const express = require('express');
+
 const cors = require('cors');
+const bodyParser = require('body-parser');
+
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-    console.log('Request sent.');
-    next();
-})
+// ====================================== Routes ========================================
 
-app.use((req, res, next) => {
-    res.status(201);
-    next();
-})
+app.post('/api/stuff', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+        message: 'Object has been created.'
+    });
+});
 
-app.use((req, res, next) => {
-    res.json({ message: 'Yep, you are in.' })
-    next();
-})
-
-app.use((req, res) => {
-    console.log('Response received.')
-})
+app.get('/api/stuff', (req, res, next) => {
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'Mon premier objet',
+        description: 'Les infos de mon premier objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        title: 'Mon deuxième objet',
+        description: 'Les infos de mon deuxième objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+    res.status(200).json(stuff);
+  });
 
 
 
