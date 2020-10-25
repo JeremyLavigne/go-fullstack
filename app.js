8ae522eab1195bf8e77623bb67a9906cbf748825
JeremyLavigne <lavigne.jeremy@post.com>
2020-10-25 14:00:30 +0100

    Implement user authentification.
    Following tuto a little too fast and not understanding all.
    
    But it is always good to do...

--- app.js
+++ app.js
@@ -13,41 +13,10 @@ mongoose.connect(`mongodb+srv://jeremy:${process.env.MONGO_PASS}@cluster0.tjm5l.
 app.use(cors());
 app.use(bodyParser.json());
 
-const Thing = require('./models/thing');
+const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
-// ====================================== Routes ========================================
-
-app.post('/api/stuff', (req, res, next) => {
-    delete req.body._id;
-    const thing = new Thing({...req.body})
-    // console.log(req.body);
-    thing.save()
-        .then(() => res.status(201).json({ message: 'Object has been created.' }))
-        .catch((error) => res.status(400).json({ error }))
-});
-
-app.get('/api/stuff', (req, res, next) => {
-    Thing.find()
-        .then((things) => res.status(200).json(things))
-        .catch((error) => res.status(400).json({ error }))
-});
-
-app.get('/api/stuff/:id', (req, res, next) => {
-    Thing.findOne({ _id: req.params.id })
-        .then((thing) => res.status(200).json(thing))
-        .catch((error) => res.status(404).json({ error }))
-})
-
-app.put('/api/stuff/:id', (req, res, next) => {
-    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
-
-app.delete('/api/stuff/:id', (req, res, next) => {
-    Thing.deleteOne({ _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-      .catch(error => res.status(400).json({ error }));
-});
+app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
