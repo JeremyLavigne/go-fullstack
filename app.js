b26accadaf8e06b1896b576d6dd59288282ac37e
JeremyLavigne <lavigne.jeremy@post.com>
2020-10-25 10:19:32 +0100

    Connect to MongoDb.
    Install dotenv and manage acces to env variables.

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
+mongoose.connect(`mongodb+srv://jeremy:${process.env.MONGO_PASS}@cluster0.tjm5l.mongodb.net/go-fullstack?retryWrites=true&w=majority`,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB.'))
+  .catch(() => console.log('Failed to connect MongoDB.'));
 
 app.use(cors());
 app.use(bodyParser.json());
